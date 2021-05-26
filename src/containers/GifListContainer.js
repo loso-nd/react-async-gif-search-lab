2fd4ae2dc8d4253207cb5b70b56a2c7d3e425d27
Nahum <losond8@gmail.com>
2021-05-26 08:20:39 -0700

    Fetched data from API, stored into state, passed into child as props to GifList, renders a 2nd component GifSearch and passes a handle submit method as props to it.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+
+class GifListContainer extends Component {
+    state = {
+        giphy : []
+    }
+
+    //fetching data from Giphy API, store first 3 gifs from response to the state, pass that state down as props the the child <GifList />
+    componentDidMount(){
+        fetch('https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
+        .then(res => res.json())
+        .then(gifs => {
+            this.setState({
+                giphy: gifs.data
+            })
+        })
+    }
+    render(){
+        return (
+            <div>
+                <h1>Anisha I like you.</h1>
+                <GifList gif={this.state.giphy} />
+            </div>
+           
+        )
+    }
+}
+
+export default GifListContainer;
